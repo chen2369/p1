b38c6df63c5421b87a1e617e55a564b6270d821d
evahahapiano <51196524+evahahapiano@users.noreply.github.com>
2019-06-02 03:27:05 +0800

    Update 抽牌.py
    改好了

--- 抽牌.py
+++ 抽牌.py
@@ -1,15 +1,24 @@
 import random
 
+class Card():
+	def __init__(self,color,num):
+		self.color = color
+		self.number = num
+
+	def showData(self):
+		return "%s-%02d"%(self.color[0:3],self.number)
+
 '''--- Class ---'''
 
 class Cardpool(object):
     def __init__(self):
-        self.cards = []
-        for c in range(6):
-            for i in range(1, 11):
-                card = "%c%d" % (chr(ord('A') + c), i)
-                self.cards.append(card)
-        random.shuffle(self.cards)
+        self.cardPile = []
+        for cl in ['RED','ORANGE','YELLOW','GREEN','BLUE','PURPLE']:
+            for nm in range(1, 11):
+                newCard = Card(cl, nm)
+                self.cardPile.append(newCard)
+
+        random.shuffle(self.cardPile)
 
         self.used_cnt = 0
 
@@ -17,8 +26,8 @@ class Cardpool(object):
         return self.used_cnt < 60
 
     def get_next(self):
-        card = self.cards[self.used_cnt]
+        newCard = self.cardPile[self.used_cnt]
         self.used_cnt += 1
-        return card
+        return newCard
 
 cardpool = Cardpool()
